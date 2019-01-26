8dd2d168f491b3e9701bb1a91ca14c0297c5be61
Carl Gold <carl.gold@zuora.com>
2019-01-26 10:15:42 -0500

    Start adding chapter 3 sqls

---
+++ examples/chap3/listing_3_1_event_count.sql
@@ -0,0 +1,9 @@
+with calc_date as (
+	select '2017-01-28'::timestamp  as the_date   
+) 
+select account_id, count(*) as n_login
+from event e inner join calc_date d on
+  e.event_time <= d.the_date
+  and e.event_time > d.the_date - interval '28 day'
+where e.event_type_id=1
+group by account_id; 
---
+++ examples/chap3/listing_3_2_multi_date_count.sql
@@ -0,0 +1,11 @@
+with date_vals AS (
+  	select i::timestamp as metric_date 
+from generate_series('2017-01-29', '2017-04-16', '7 day'::interval) i 
+)
+select account_id, metric_date, count(*) as n_login 
+from event e inner join date_vals d
+on e.event_time < metric_date
+and e.event_time >= metric_date - interval '28 day'
+where e.event_type_id=18    
+group by account_id, metric_date
+order by account_id, metric_date;
