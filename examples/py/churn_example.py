2074ecf26fcdbc353c2bee040a40863fb13793aa
Carl Gold <carl.gold@zuora.com>
2019-03-11 08:14:25 -0700

    Check the example name and add a chapter setting

--- examples/py/churn_example.py
+++ examples/py/churn_example.py
@@ -2,14 +2,20 @@ from postgres import Postgres
 import json
 import pandas as pd
 
+one_example=None
+one_chapter=None
+
 schema = 'b'
 # schema = 'k'
 # schema = 'v'
 save_path = '../../../fight-churn-output/' + schema + '/'
 
 # one_example='listing_4_1_ongoing_active_periods'
-one_example='listing_4_5_data_set_create_template'
-# one_example=None
+one_example='listing_2_5_churn_rate_scaled'
+
+one_chapter='chap2'
+
+
 
 flat_metric_bind = 'FLAT_METRIC_SELECT'
 flat_metric_template = ', sum(case when metric_name_id=%d then metric_value else 0 end) as %s'
@@ -23,9 +29,16 @@ def generate_flat_metric_sql(db):
 with open('../conf/%s_examples.json' % schema, 'r') as myfile:
 	param_dict=json.loads(myfile.read())
 
+if one_chapter is not None:
+	assert one_chapter in param_dict, 'No chapter %s' % one_chapter
+if one_example is not None and one_chapter is not None:
+	assert one_example in param_dict[one_chapter], 'No example %s in chapter %s' % (one_example, one_chapter)
+
 db = Postgres("postgres://postgres:churn@localhost/postgres")
 
 for chapter in param_dict.keys():
+	if one_chapter is not None and chapter != one_chapter:
+		continue
 
 	for idx, example in enumerate(param_dict[chapter].keys()):
 		if one_example is not None and example != one_example:
@@ -47,8 +60,9 @@ for chapter in param_dict.keys():
 				db.run(sql)
 			elif param_dict[chapter][example].get('mode', 'run') == 'one':
 				res = db.one(sql)
+				print(res)
 			elif param_dict[chapter][example].get('mode', 'run') == 'all':
 				res = db.all(sql)
 				df = pd.DataFrame(res)
 				print('Saving...')
-				df.to_csv(save_path + example + ' .csv')
+				df.to_csv(save_path + example + ' .csv',index=False)
