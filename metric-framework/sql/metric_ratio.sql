4622f77b0004d750a02d86567582bcceba55453d
Carl Gold <carl.gold@zuora.com>
2019-03-18 09:49:50 -0600

    Add ratio metrics to the metric framework.

---
+++ metric-framework/sql/metric_ratio.sql
@@ -0,0 +1,26 @@
+set search_path = '%schema';
+
+with num_metric as (
+	select account_id, metric_time, metric_value as num_value
+	from metric m inner join metric_name n on n.metric_name_id=m.metric_name_id
+	and n.metric_name = '%num_metric'
+	and metric_time between '%from_date' and '%to_date'
+), den_metric as (
+	select account_id, metric_time, metric_value as den_value
+	from metric m inner join metric_name n on n.metric_name_id=m.metric_name_id
+	and n.metric_name = '%den_metric'
+	and metric_time between '%from_date' and '%to_date'
+)
+
+insert into metric (account_id,metric_time,metric_name_id,metric_value)
+
+select d.account_id, d.metric_time, %metric_name_id,
+	case when den_value > 0
+	    then coalesce(num_value,0.0)/den_value
+	    else %fill_val
+    end as ask_success_rate
+from den_metric d  left outer join num_metric n
+	on n.account_id=d.account_id
+	and n.metric_time=d.metric_time;
+
+insert into metric_name (metric_name_id,metric_name) values (%metric_name_id,'%metric_name_val');
