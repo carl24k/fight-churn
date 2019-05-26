133383dbfc0826003556bf40a6567f6087a04594
Gold, Carl <carl.gold@zuora.com>
2019-05-26 12:56:39 -0700

    Make metrics run from a function

--- metric-framework/py/metric_calc.py
+++ metric-framework/py/metric_calc.py
@@ -2,72 +2,87 @@ from postgres import Postgres
 import json
 from metric_util import metricIdSql
 import os
+import sys
 
 bind_char='%'
 
 
-schema='churnsim2'
+def calculate_metrics(schema,run_mets):
 
-run_mets=None
-# run_mets=['account_tenure','post_per_month']
-
-
-
-with open('../conf/%s_metrics.json' % schema, 'r') as myfile:
-	metric_dict=json.loads(myfile.read())
+	with open('../conf/%s_metrics.json' % schema, 'r') as myfile:
+		metric_dict=json.loads(myfile.read())
 
 
-from_date=metric_dict['date_range']['from_date']
-to_date=metric_dict['date_range']['to_date']
+	from_date=metric_dict['date_range']['from_date']
+	to_date=metric_dict['date_range']['to_date']
 
-db = Postgres("postgres://%s:%s@localhost/%s" % (
-os.environ['CHURN_DB_USER'], os.environ['CHURN_DB_PASS'], os.environ['CHURN_DB']))
+	db = Postgres("postgres://%s:%s@localhost/%s" % (
+	os.environ['CHURN_DB_USER'], os.environ['CHURN_DB_PASS'], os.environ['CHURN_DB']))
 
-if run_mets is None:
-	print('TRUNCATING *Metrics* in schema -> %s <-  ...' % schema)
-	if input("are you sure? (enter %s to proceed) " % schema) == schema:
-		db.run('truncate table %s.metric' % schema)
-		db.run('truncate table %s.metric_name' % schema)
-	else:
-		exit(0)
-else:
-	if isinstance(run_mets,str): run_mets=[run_mets]
-	if len(run_mets)>1:
-		print('DELETING * %d * Metrics in schema -> %s <-  ...' % (len(run_mets),schema))
-		if input("are you sure? (enter %s to proceed) " % schema) != schema:
+	if run_mets is None:
+		print('TRUNCATING *Metrics* in schema -> %s <-  ...' % schema)
+		if input("are you sure? (enter %s to proceed) " % schema) == schema:
+			db.run('truncate table %s.metric' % schema)
+			db.run('truncate table %s.metric_name' % schema)
+		else:
 			exit(0)
-	for m in run_mets:
-		id =db.one(metricIdSql(schema,m))
-		if id is not None:
-			deletSql="delete from %s.metric where metric_name_id=%d and metric_time between '%s'::timestamp and '%s'::timestamp"  \
-				   % (schema,id,from_date,to_date)
-			print('Clearing old values: ' + deletSql)
-			db.run(deletSql)
+	else:
+		if isinstance(run_mets,str): run_mets=[run_mets]
+		if len(run_mets)>1:
+			print('DELETING * %d * Metrics in schema -> %s <-  ...' % (len(run_mets),schema))
+			if input("are you sure? (enter %s to proceed) " % schema) != schema:
+				exit(0)
+		for m in run_mets:
+			id =db.one(metricIdSql(schema,m))
+			if id is not None:
+				deletSql="delete from %s.metric where metric_name_id=%d and metric_time between '%s'::timestamp and '%s'::timestamp"  \
+					   % (schema,id,from_date,to_date)
+				print('Clearing old values: ' + deletSql)
+				db.run(deletSql)
 
 
-for metric in metric_dict.keys():
-	if (run_mets is not None and metric not in run_mets) or metric == 'date_range':
-		continue
+	for metric in metric_dict.keys():
+		if (run_mets is not None and metric not in run_mets) or metric == 'date_range':
+			continue
 
-	id = db.one(metricIdSql(schema, metric))
-	if id is None:
-		id=db.one('select max(metric_name_id)+1 from %s.metric_name' % schema)
-		if id is None: id=0
-		insertNameSql = "insert into %s.metric_name (metric_name_id,metric_name) values (%d,'%s')" % (schema,id,metric)
-		db.run(insertNameSql)
+		id = db.one(metricIdSql(schema, metric))
+		if id is None:
+			id=db.one('select max(metric_name_id)+1 from %s.metric_name' % schema)
+			if id is None: id=0
+			insertNameSql = "insert into %s.metric_name (metric_name_id,metric_name) values (%d,'%s')" % (schema,id,metric)
+			db.run(insertNameSql)
 
-	print('Inserting metric %s.%s as id %d' % (schema,metric,id))
-	with open('../sql/%s.sql' % metric_dict[metric]['sql'], 'r') as myfile:
-		sql=myfile.read().replace('\n', ' ')
-	params=metric_dict[metric]
-	params['metric_name_val']=metric
-	params['schema']=schema
-	params['from_date']=from_date
-	params['to_date']=to_date
-	params['metric_name_id']=id
-	for p in params.keys():
-		sql=sql.replace(bind_char+p,str(params[p]))
-	print(sql)
+		print('Inserting metric %s.%s as id %d' % (schema,metric,id))
+		with open('../sql/%s.sql' % metric_dict[metric]['sql'], 'r') as myfile:
+			sql=myfile.read().replace('\n', ' ')
+		params=metric_dict[metric]
+		params['metric_name_val']=metric
+		params['schema']=schema
+		params['from_date']=from_date
+		params['to_date']=to_date
+		params['metric_name_id']=id
+		for p in params.keys():
+			sql=sql.replace(bind_char+p,str(params[p]))
+		print(sql)
 
-	db.run(sql)
+		db.run(sql)
+
+'''
+####################################################################################################
+The main script for calculating Fight Churn With Data metrics in batch: If there are command line arguments, 
+use them. Otherwise defaults are hard coded
+
+'''
+
+if __name__ == "__main__":
+
+	schema = 'churnsim2'
+	run_mets = None
+	# run_mets=['account_tenure','post_per_month']
+
+	if len(sys.argv)>=3:
+		schema=sys.argv[1]
+		run_mets=sys.argv[2:]
+
+	calculate_metrics(schema,run_mets)
 
