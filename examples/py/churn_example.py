4bda3d4d120f824e6d7b8bb26f5ef05c36dbab72
Gold, Carl <carl.gold@zuora.com>
2019-05-26 10:17:41 -0700

    Refactor the main function to load the parameters in a function

--- examples/py/churn_example.py
+++ examples/py/churn_example.py
@@ -2,17 +2,18 @@ from postgres import Postgres
 import json
 import pandas as pd
 from importlib import import_module
+from copy import copy
 import re
 import os
 import sys
+
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 print_num_rows=5
 
-flat_metric_bind = 'FLAT_METRIC_SELECT'
 
-def generate_flat_metric_sql(db):
+def generate_flat_metric_sql(db, schema):
     '''
     Helper function for saving a data set for analysis. This flattens the metrics table to form a data set and it
     needs to add a sql clause for every metric in the schema.  This does that by actually querying the metric, table,
@@ -28,43 +29,45 @@ def generate_flat_metric_sql(db):
     return sql
 
 
-def sql_example(param_dict, chapter, example):
+def sql_example(param_dict):
 
-    chap_params = param_dict[chapter]['params']
+    flat_metric_bind = 'FLAT_METRIC_SELECT'
 
-    with open('../%s/%s.sql' % (chapter, example), 'r') as myfile:
+    with open('../chap%d/%s.sql' % (param_dict['chapter'], param_dict['name']), 'r') as myfile:
         db = Postgres("postgres://%s:%s@localhost/%s" % (os.environ['CHURN_DB_USER'],os.environ['CHURN_DB_PASS'],os.environ['CHURN_DB']))
 
-        sql = "set search_path = '%s'; " % schema;
+        sql = "set search_path = '%s'; " % param_dict['schema'];
         sql = sql + myfile.read()
 
-        for p in chap_params.keys():
-            if p=='type': continue
-            sql = sql.replace(p, chap_params[p])
-        param_keys = [p for p in param_dict[chapter][example].keys() if p not in ('listing', 'mode')]
+        param_keys = [p for p in param_dict.keys() if p not in ('listing', 'mode','type')]
         for p in param_keys:
-            sql = sql.replace(p, str(param_dict[chapter][example][p]))
+            sql = sql.replace(p, str(param_dict[p]))
         if flat_metric_bind in sql:
-            sql = sql.replace(flat_metric_bind, generate_flat_metric_sql(db))
+            sql = sql.replace(flat_metric_bind, generate_flat_metric_sql(db,param_dict['schema']))
 
         print('SQL:\n----------\n'+sql+'\n----------\nRESULT:')
         sql = sql.replace('\n', ' ')
 
-        mode = param_dict[chapter][example]['mode'] if 'mode' in param_dict[chapter][example] else chap_params['mode']
-        if mode == 'run':
+        if  param_dict['mode']  == 'run':
             db.run(sql)
-        elif mode == 'one':
+        elif  param_dict['mode']  == 'one':
             res = db.one(sql)
             print(res)
-        elif mode == 'top' or mode == 'save':
+        elif  param_dict['mode']  == 'top' or param_dict['mode'] == 'save':
             res = db.all(sql)
             df = pd.DataFrame(res)
-            if mode == 'save':
-                csv_path=save_path + example + '.csv'
+            if  param_dict['mode']  == 'save':
+                save_path = '../../../fight-churn-output/' + param_dict['schema'] + '/'
+                os.makedirs(save_path,exist_ok=True)
+                csv_path=save_path +  param_dict['name']  + '.csv'
                 print('Saving: %s' % csv_path)
                 df.to_csv(csv_path, index=False)
             else:
                 print(df.head(print_num_rows))
+        else:
+            print('Unknown run mode for SQL example')
+            exit(-4)
+
 
 def python_example(param_dict,chapter,example):
 
@@ -86,20 +89,7 @@ def python_example(param_dict,chapter,example):
     else:
         print('Bad function name')
 
-
-if __name__ == "__main__":
-
-    schema = 'churnsim2'
-    chapter = 4
-    listing = 'A'
-
-    if len(sys.argv)==4:
-        schema=sys.argv[1]
-        chapter=sys.argv[2]
-        listing=sys.argv[3]
-
-    save_path = '../../../fight-churn-output/' + schema + '/'
-    os.makedirs(save_path,exist_ok=True)
+def load_and_check_listing_params(schema, chapter, listing):
 
     chapter_key='chap{}'.format(chapter)
     listing_prefix='listing_{c}_{l}_'.format(c=chapter,l=listing)
@@ -116,23 +106,47 @@ if __name__ == "__main__":
         print('No params for chapter %d in %s_examples.json' % (chapter,schema))
         exit(-2)
 
-    found_listing = False
+    listing_params = copy(param_dict[chapter_key]['params'])
+
     for listing_name in param_dict[chapter_key].keys():
 
         if listing_name=='params' or not listing_prefix in listing_name:
             continue
 
-        print('\nRunning %s listing %s on schema %s' % (chapter_key,listing_name,schema))
-        found_listing=True
-        type = param_dict[chapter_key][listing_name].get('type', param_dict[chapter_key]['params']['type']) # chap params should always have type
-        if type=='sql':
-            sql_example(param_dict,chapter_key,listing_name)
-        elif type=='py':
-            python_example(param_dict,chapter_key,listing_name)
-        else:
-            raise Exception('Unsupported type %s' % type)
-        exit(0)
+        listing_params['schema'] = schema
+        listing_params['name'] = listing_name
+        listing_params['chapter']=chapter
+        listing_params['listing']=listing
+        if len(param_dict[chapter_key][listing_name])>0:
+            for k in param_dict[chapter_key][listing_name].keys():
+                listing_params[k]=param_dict[chapter_key][listing_name][k]
 
-    if not found_listing:
-        print('No params for listing %d, chapter %d in %s_examples.json' % (listing,chapter,schema))
-        exit(-3)
+        return listing_params
+
+    print('No params for listing %d, chapter %d in %s_examples.json' % (listing,chapter,schema))
+    exit(-3)
+
+
+if __name__ == "__main__":
+
+    schema = 'churnsim2'
+    chapter = 2
+    listing = 1
+
+    if len(sys.argv)==4:
+        schema=sys.argv[1]
+        chapter=sys.argv[2]
+        listing=sys.argv[3]
+
+    listing_params = load_and_check_listing_params(schema,chapter,listing)
+
+    print('\nRunning %d listing %s on schema %s' % (chapter,listing_params['name'],schema))
+
+    type = listing_params.get('type', listing_params['type']) # chap params should always have type
+    if type=='sql':
+        sql_example(listing_params)
+    elif type=='py':
+        python_example(listing_params)
+    else:
+        raise Exception('Unsupported type %s' % type)
+
