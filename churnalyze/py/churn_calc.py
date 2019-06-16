a2286ef32260c2bddc321d92ab754a7fa3f01c6a
Gold, Carl <carl.gold@zuora.com>
2019-06-16 11:22:33 -0700

    More refactoring and the churn stats program now works on one case study data set

--- churnalyze/py/churn_calc.py
+++ churnalyze/py/churn_calc.py
@@ -1,53 +1,70 @@
 import os
 import pandas as pd
 import numpy as np
+import json
 
-from churn_const import out_col, no_plot, save_path, schema_data_dict, skip_metrics,key_cols,max_clips,min_valid
 
 class ChurnCalculator:
+    # misc constants set as class variables
+    key_cols = ['account_id', 'observation_date']
+    churn_out_col = 'is_churn'
+    save_path_base = '../../../fight-churn-output/'
+    load_mat_file = 'load_mat'
+    no_plot_cols = list(key_cols)
+    no_plot_cols.append(churn_out_col)
 
-    def __init__(self,schema):
-        self.schema=schema
-        self.churn_data=None
+    def __init__(self, schema):
+        self.schema = schema
+        self.data_set_name = None
+        self.churn_data = None
+        with open('../conf/%s_churnalyze.json' % schema, 'r') as myfile:
+            self.conf = json.loads(myfile.read())
+        self.data_set_name = self.get_conf('dataset')
         self.data_load()
-        self.metric_columns=self.churn_metric_columns()
         # derived data created by calling functions
-        self.summary=None
-        self.data_scores=None
-        self.skewed_columns=None
-        self.churn_data_reduced=None
-        self.reduced_cols=None
+        self.summary = None
+        self.data_scores = None
+        self.skewed_columns = None
+        self.churn_data_reduced = None
+        self.reduced_cols = None
 
-        # misc constants set as member variables
-        self.key_cols = ['account_id', 'observation_date']
-        self.out_col = 'is_churn'
-        self.no_plot = list(key_cols)
-        self.no_plot.append(out_col)
-        self.save_path_base = '../../../fight-churn-output/'
-        self.load_mat_file = 'load_mat'
+    def get_conf(self, name):
+        if self.data_set_name is not None:
+            if self.data_set_name in self.conf:
+                if name in self.conf[self.data_set_name]:
+                    return self.conf[self.data_set_name][name]
+        if name in self.conf['default']:
+            return self.conf['default'][name]
+        return None
 
     def data_load(self):
-        data_file = schema_data_dict[self.schema]
-        schema_save_path = save_path(self.schema) + data_file
-        self.churn_data = pd.read_csv(schema_save_path + '.csv')
-        if data_file in skip_metrics:
-            self.churn_data.drop(skip_metrics[data_file], axis=1,inplace=True)
-        self.churn_data.set_index(key_cols,inplace=True)
+        data_set_path = self.save_path()
+        self.churn_data = pd.read_csv(data_set_path)
+        skip_metrics = self.get_conf('skip_metrics')
+        if len(skip_metrics) > 0:
+            self.churn_data.drop(skip_metrics, axis=1, inplace=True)
+        self.churn_data.set_index(self.key_cols, inplace=True)
 
-        for metric in max_clips.keys():
-            if metric in self.churn_data.columns.values:
-                self.churn_data[metric].clip(upper=max_clips[metric], inplace=True)
+        max_clips = self.get_conf('max_clips')
+        if max_clips is not None and isinstance(max_clips, dict):
+            for metric in max_clips.keys():
+                if metric in self.churn_data.columns.values:
+                    self.churn_data[metric].clip(upper=max_clips[metric], inplace=True)
 
-        print('%s size before validation of columns: %d' % (data_file, self.churn_data.shape[0]))
+        print('%s size before validation of columns: %d' % (self.data_set_name, self.churn_data.shape[0]))
 
-        for metric in min_valid.keys():
-            if metric.lower() in self.churn_data.columns.values:
-                self.churn_data=self.churn_data[self.churn_data[metric.lower()]>min_valid[metric]]
+        min_valid = self.get_conf('min_valid')
+        if min_valid is not None and isinstance(min_valid, dict):
+            for metric in min_valid.keys():
+                if metric.lower() in self.churn_data.columns.values:
+                    self.churn_data = self.churn_data[self.churn_data[metric.lower()] > min_valid[metric]]
 
-        print('Loaded %s, size=%dx%d with columns:' % (data_file, self.churn_data.shape[0], self.churn_data.shape[1]))
+        self.metric_columns = self.churn_metric_columns()
+        print('Loaded %s, size=%dx%d with columns:' % (
+        self.data_set_name, self.churn_data.shape[0], self.churn_data.shape[1]))
+        print('|'.join(self.metric_columns))
 
-
-    def behavioral_cohort_plot_data(self,var_to_plot,nbin=10,out_col=out_col):
+    def behavioral_cohort_plot_data(self, var_to_plot, nbin=10, out_col=churn_out_col):
         """
         Make a data frame with two columns prepared to be the plot points for a behavioral cohort plot.
         The data is binned into 10 ordered bins, and the mean value of the metric and the churn rate are calculated
@@ -64,8 +81,7 @@ class ChurnCalculator:
 
         return plot_frame
 
-
-    def dataset_stats(self,save_path=None):
+    def dataset_stats(self, save_path=None):
         """
         Take basic stats of the data set.  Saving it is optional.
         :param metric_cols: Columns which are metrics (and will have stats taken)
@@ -74,9 +90,8 @@ class ChurnCalculator:
         """
 
         if self.summary is None:
-
-            self.summary=self.churn_data[self.metric_cols].describe()
-            self.summary=self.summary.transpose()
+            self.summary = self.churn_data[self.metric_columns].describe()
+            self.summary = self.summary.transpose()
 
             self.summary['skew'] = self.churn_data.skew()
             self.summary['1%'] = self.churn_data.quantile(q=0.01)
@@ -87,16 +102,17 @@ class ChurnCalculator:
             self.summary['99%'] = self.churn_data.quantile(q=0.99)
             self.summary['nonzero'] = self.churn_data.astype(bool).sum(axis=0) / self.churn_data.shape[0]
 
-            self.summary = self.summary[ ['count','nonzero','mean','std','skew','min','1%','5%','10%','25%','50%','75%','90%','95%','99%','max'] ]
+            self.summary = self.summary[
+                ['count', 'nonzero', 'mean', 'std', 'skew', 'min', '1%', '5%', '10%', '25%', '50%', '75%', '90%', '95%',
+                 '99%', 'max']]
 
         if save_path is not None:
             # Adding churn % stats in a saved version, but not for further analysis
-            churn_stat=self.churn_data['is_churn'].astype(int).describe()
-            churn_stat.to_csv(save_path+'_churnrate.csv',header=True)
-            self.summary.to_csv(save_path+'_summary.csv',header=True)
+            churn_stat = self.churn_data['is_churn'].astype(int).describe()
+            churn_stat.to_csv(save_path + '_churnrate.csv', header=True)
+            self.summary.to_csv(save_path + '_summary.csv', header=True)
 
         return self.summary
-
 
     def normalize_skewscale(self, log_scale_skew_thresh=4):
         """
@@ -108,46 +124,50 @@ class ChurnCalculator:
         :return: Data frame with the normalized columns and which columns received the log transform
         """
         if self.data_scores is None:
-            self.dataset_stats() # make sure summary is created
-            self.data_scores=self.churn_data[self.metric_columns].copy()
-            skewed_columns=(self.summary['skew']>log_scale_skew_thresh) & (self.summary['min'] >= 0)
-            for col,do_scale in skewed_columns.iteritems():
+            self.dataset_stats()  # make sure summary is created
+            self.data_scores = self.churn_data[self.metric_columns].copy()
+            skewed_columns = (self.summary['skew'] > log_scale_skew_thresh) & (self.summary['min'] >= 0)
+            for col, do_scale in skewed_columns.iteritems():
                 if do_scale:
-                    self.data_scores[col]=np.log(1.0+self.data_scores[col])
+                    self.data_scores[col] = np.log(1.0 + self.data_scores[col])
 
-            self.data_scores=(self.data_scores-self.data_scores.mean())/self.data_scores.std()
-            self.data_scores['is_churn']=self.churn_data['is_churn']
+            self.data_scores = (self.data_scores - self.data_scores.mean()) / self.data_scores.std()
+            self.data_scores['is_churn'] = self.churn_data['is_churn']
 
         return self.data_scores, self.skewed_columns
 
-
     def churn_metric_columns(self):
-        all_columns=list(self.hurn_data.columns.values)
-        plot_columns = [c for c in all_columns if c not in no_plot]
+        all_columns = list(self.churn_data.columns.values)
+        plot_columns = [c for c in all_columns if c not in ChurnCalculator.no_plot_cols]
         return plot_columns
 
     def group_behaviors(self):
 
-        self.normalize_skewscale() # make sure scores are created
+        self.normalize_skewscale()  # make sure scores are created
 
-        load_mat_df = pd.read_csv(save_path(self.schema, self.load_mat_file), index_col=0)
+        load_mat_df = pd.read_csv(self.save_path(ChurnCalculator.load_mat_file), index_col=0)
         num_weights = load_mat_df.astype(bool).sum(axis=0)
         load_mat_df = load_mat_df.loc[:, num_weights > 1]
         grouped_columns = ['Metric Group %d' % (d + 1) for d in range(0, load_mat_df.shape[1])]
-        self.churn_data_reduced = pd.DataFrame(np.matmul(self.data_scores[self.metric_columns].to_numpy(), load_mat_df.to_numpy()),
-                                          columns=grouped_columns, index=self.churn_data.index)
+        self.churn_data_reduced = pd.DataFrame(
+            np.matmul(self.data_scores[self.metric_columns].to_numpy(), load_mat_df.to_numpy()),
+            columns=grouped_columns, index=self.churn_data.index)
         self.churn_data_reduced['is_churn'] = self.churn_data['is_churn']
-        self.reduced_cols=grouped_columns
+        self.reduced_cols = grouped_columns
 
         # churn_data = churn_data_reduced
         # plot_columns = grouped_columns
         # the_one_plot = None
         # skewed_columns = {c: False for c in plot_columns}
 
-    def save_path(self,file_name=None):
-        save_path=self.save_path_base + self.schema + '/'
+    def save_path(self, file_name=None,ext='csv'):
+        save_path = ChurnCalculator.save_path_base + self.schema + '/'
         os.makedirs(save_path, exist_ok=True)
-        if file_name is None:
-            return save_path
+        if self.data_set_name is None and file_name is not None:
+            return save_path + '/' + file_name + '.' + ext
+        elif file_name is  None and self.data_set_name is not None:
+            return save_path + '/' + self.data_set_name + '.' + ext
+        elif file_name is  not None and self.data_set_name is not None:
+            return save_path + '/' + self.data_set_name + '_' + file_name + '.' + ext
         else:
-            return save_path + '/' + schema_data_dict[self.schema] + '_' + file_name + '.csv'
+            raise Exception("Must provide file name or previously set the datset ")
--- churnalyze/py/churn_stats.py
+++ churnalyze/py/churn_stats.py
@@ -1,31 +1,34 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+# import pandas as pd
+import sys
+# import matplotlib.pyplot as plt
 
-import churn_calc as cc
-from churn_const import save_path, key_cols, no_plot, schema_data_dict
-
-# schema = 'b'
-# schema = 'v'
-schema = 'k'
-schema = 'churnsim2'
-
-data_file = schema_data_dict[schema]
-schema_save_path = save_path(schema)+data_file
+from churn_calc import ChurnCalculator
+# from churn_const import  schema_data_dict
 
 
 def main():
 
-    churn_data = cc.data_load(schema)
 
-    stat_columns = cc.churn_metric_columns(churn_data.columns.values)
+    schema = 'b'
+    run_mets = None
+    # Example of running just a few metrics - uncomment this line...
+    # run_mets=['account_tenure','post_per_month']
 
-    summary = cc.dataset_stats(churn_data,stat_columns, save_path=schema_save_path)
+    if len(sys.argv) >= 2:
+        schema = sys.argv[1]
 
-    data_scores, skewed_columns = cc.normalize_skewscale(churn_data, stat_columns, summary)
+    churn_calc = ChurnCalculator(schema)
+    churn_calc.dataset_stats(churn_calc.save_path('stats'))
 
-    corr = data_scores.corr()
-
-    corr.to_csv(schema_save_path + '_corr.csv')
+    # stat_columns = cc.churn_metric_columns(churn_data.columns.values)
+    #
+    # summary = cc.dataset_stats(churn_data,stat_columns, save_path=schema_save_path)
+    #
+    # data_scores, skewed_columns = cc.normalize_skewscale(churn_data, stat_columns, summary)
+    #
+    # corr = data_scores.corr()
+    #
+    # corr.to_csv(schema_save_path + '_corr.csv')
 
 
 if __name__ == "__main__":
