0f5f11d02fb8d25161c49c2310cb4f66ea7763d9
Gold, Carl <carl.gold@zuora.com>
2019-06-20 10:05:46 -0700

    rename variables

--- listings/chap6/listing_6_1_metric_pair_plot.py
+++ listings/chap6/listing_6_1_metric_pair_plot.py
@@ -6,13 +6,13 @@ def metric_pair_plot(data_set_path='', metric1='',metric2='',save_path='example.
     churn_data = pd.read_csv(data_set_path)
     churn_data.set_index(['account_id','observation_date'],inplace=True)
 
-    met1_data = churn_data[metric1]
-    met2_data = churn_data[metric2]
+    met1_series = churn_data[metric1]
+    met2_series = churn_data[metric2]
 
-    corr = met1_data.corr(met2_data)
+    corr = met1_series.corr(met2_series)
 
     plt.figure(figsize=(6, 6))
-    plt.scatter(met1_data, met2_data, marker='.')
+    plt.scatter(met1_series, met2_series, marker='.')
     plt.xlabel(metric1)
     plt.ylabel(metric2)
     plt.title('Correlation = %.2f' % corr)
