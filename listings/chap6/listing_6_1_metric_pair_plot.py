a13b183e0288dda09ad4a8c4f199eba1a53c5f99
Gold, Carl <carl.gold@zuora.com>
2019-06-21 13:33:19 -0700

    Remove unnecessary size and add better output name

--- listings/chap6/listing_6_1_metric_pair_plot.py
+++ listings/chap6/listing_6_1_metric_pair_plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def metric_pair_plot(data_set_path='', metric1='',metric2='',save_path='example.png'):
+def metric_pair_plot(data_set_path='', metric1='',metric2='',save_path='./'):
 
     churn_data = pd.read_csv(data_set_path)
     churn_data.set_index(['account_id','observation_date'],inplace=True)
@@ -11,12 +11,13 @@ def metric_pair_plot(data_set_path='', metric1='',metric2='',save_path='example.
 
     corr = met1_series.corr(met2_series)
 
-    plt.figure(figsize=(6, 6))
     plt.scatter(met1_series, met2_series, marker='.')
+
     plt.xlabel(metric1)
     plt.ylabel(metric2)
     plt.title('Correlation = %.2f' % corr)
     plt.tight_layout()
-    plt.savefig(save_path)
-    print('Saving plot to %s' % save_path)
+    save_name = save_path + '/' + metric1 + '_vs_' + metric2 + '.png'
+    plt.savefig(save_name )
+    print('Saving plot to %s' % save_name)
     plt.close()
