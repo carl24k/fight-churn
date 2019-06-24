bb326cd38bee9b5b6ff901d562c3009654b56c3a
Gold, Carl <carl.gold@zuora.com>
2019-06-24 08:52:51 -0700

    Bug fixes in apply metric groups

--- listings/chap6/listing_6_3_apply_metric_groups.py
+++ listings/chap6/listing_6_3_apply_metric_groups.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import numpy as np
 
-def apply_metric_groups(data_set_path='',load_mat_path='',save=True):
+def apply_metric_groups(data_set_path='',save=True):
 
     score_save_path = data_set_path.replace('.csv', '_scores.csv')
     churn_data = pd.read_csv(score_save_path)
     churn_data.set_index(['account_id','observation_date'],inplace=True)
     group_data = churn_data.copy()
-    group_data.drop('is_churn',axis=1)
+    group_data = group_data.drop('is_churn',axis=1)
 
-    load_mat_df = pd.read_csv(load_mat_path, index_col=0)
-    num_weights = load_mat_df.astype(bool).sum(axis=0)
-    load_mat_df = load_mat_df.loc[:, num_weights > 1]
-    grouped_column_names = ['Metric Group %d' % (d + 1) for d in range(0, load_mat_df.shape[1])]
+    load_mat_df = pd.read_csv(data_set_path.replace('.csv', '_load_mat.csv'), index_col=0)
+    grouped_column_names = ['metric_group_%d' % (d + 1) for d in range(0, load_mat_df.shape[1])]
 
     grouped_ndarray = np.matmul(group_data.to_numpy(), load_mat_df.to_numpy())
 
