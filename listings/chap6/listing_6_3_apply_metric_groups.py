784d439e8fa7fd37c8848e5be256705149e58e2a
Gold, Carl <carl.gold@zuora.com>
2019-07-17 06:39:06 -0700

    Cleanup for readability in the book and update the save path to the new convention

--- listings/chap6/listing_6_3_apply_metric_groups.py
+++ listings/chap6/listing_6_3_apply_metric_groups.py
@@ -5,14 +5,14 @@ from listing_5_3_metric_scores import metric_scores
 def apply_metric_groups(data_set_path='',save=True):
 
     score_data = metric_scores(data_set_path)
-    group_data = score_data.drop('is_churn',axis=1)
+    data_2group = score_data.drop('is_churn',axis=1)
 
     load_mat_df = pd.read_csv(data_set_path.replace('.csv', '_load_mat.csv'), index_col=0)
-    # assert load_mat_df.index.values in group_data.columns.values and \
-    #        group_data.columns.values in load_mat_df.index.values, 'Columns of data and load matrix do not match'
-    group_data = group_data[load_mat_df.index.values]
-    grouped_ndarray = np.matmul(group_data.to_numpy(), load_mat_df.to_numpy())
+    load_mat_ndarray = load_mat_df.to_numpy()
 
+    # Make sure the data is in the same column order as the rows of the loading matrix
+    ndarray_2group = data_2group[load_mat_df.index.values].to_numpy()
+    grouped_ndarray = np.matmul(ndarray_2group, load_mat_ndarray)
 
     grouped_column_names = ['metric_group_%d' % (d + 1) for d in range(0, load_mat_df.shape[1])]
     churn_data_grouped = pd.DataFrame(grouped_ndarray,columns=grouped_column_names, index=score_data.index)
@@ -21,6 +21,8 @@ def apply_metric_groups(data_set_path='',save=True):
     churn_data_grouped['is_churn'] = score_data['is_churn']
 
     if save:
-        churn_data_grouped.to_csv(data_set_path.replace('.csv', '_group_scores.csv'),header=True)
+        save_path = data_set_path.replace('.csv', '_group_scores.csv')
+        churn_data_grouped.to_csv(save_path,header=True)
+        print('Saved grouped data  to ' + save_path)
 
     return churn_data_grouped
