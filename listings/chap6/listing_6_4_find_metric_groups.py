6451b98ee07821aa781132fa54ec8bf82191d39d
Gold, Carl <carl.gold@zuora.com>
2019-07-17 06:30:17 -0700

    Refactor HC listing to multiple functions

--- listings/chap6/listing_6_4_find_metric_groups.py
+++ listings/chap6/listing_6_4_find_metric_groups.py
@@ -5,34 +5,28 @@ from scipy.cluster.hierarchy import linkage, fcluster
 from scipy.spatial.distance import squareform
 from listing_5_3_metric_scores import metric_scores
 
-def find_metric_groups(data_set_path='',group_corr_thresh=0.5,save=True):
 
-    # Load data and convert to scores
-    score_data = metric_scores(data_set_path)
-    score_data.drop('is_churn',axis=1,inplace=True)
-    metric_columns = list(score_data.columns.values)
-
-    # Find the clusters
-    corr = score_data.corr()
+def find_correlation_clusters(corr,corr_thresh):
     dissimilarity = 1.0 - corr
     hierarchy = linkage(squareform(dissimilarity), method='single')
-    thresh = 1.0 - group_corr_thresh
-    labels = fcluster(hierarchy, thresh, criterion='distance')
-    clusters = set(labels) # The unique list of the group labels
+    diss_thresh = 1.0 - corr_thresh
+    labels = fcluster(hierarchy, diss_thresh, criterion='distance')
+    return labels
 
-    # Relabel the clusters so the cluster with the most columns is first
+def relabel_clusters(labels,metric_columns):
     cluster_count = Counter(labels)
     cluster_order = {cluster[0]: idx for idx, cluster in enumerate(cluster_count.most_common())}
     relabeled_clusters = [cluster_order[l] for l in labels]
-    relabeled_count = Counter(relabeled_clusters)
-    labeled_columns = pd.DataFrame({'group': relabeled_clusters, 'column': metric_columns}).sort_values(
+    relabled_count = Counter(relabeled_clusters)
+    labeled_column_df = pd.DataFrame({'group': relabeled_clusters, 'column': metric_columns}).sort_values(
         ['group', 'column'], ascending=[True, True])
+    return labeled_column_df, relabled_count
 
-    # Create the loading matrix
-    load_mat = np.zeros((len(metric_columns), len(clusters)))
-    for row in labeled_columns.iterrows():
+def make_load_matrix(labeled_column_df,metric_columns,relabled_count):
+    load_mat = np.zeros((len(metric_columns), len(relabled_count)))
+    for row in labeled_column_df.iterrows():
         orig_col = metric_columns.index(row[1][1])
-        load_mat[orig_col, row[1][0]] = 1.0 / float(relabeled_count[row[1][0]])
+        load_mat[orig_col, row[1][0]] = 1.0 / float(relabled_count[row[1][0]])
     loadmat_df = pd.DataFrame(load_mat, index=metric_columns, columns=[d for d in range(0, load_mat.shape[1])])
     loadmat_df['name'] = loadmat_df.index
     sort_cols = list(loadmat_df.columns.values)
@@ -40,10 +34,20 @@ def find_metric_groups(data_set_path='',group_corr_thresh=0.5,save=True):
     sort_order[-1] = True
     loadmat_df = loadmat_df.sort_values(sort_cols, ascending=sort_order)
     loadmat_df = loadmat_df.drop('name', axis=1)
+    return loadmat_df
 
-    # save if desired
+def find_metric_groups(data_set_path='',group_corr_thresh=0.5,save=True):
+
+    score_data = metric_scores(data_set_path).drop('is_churn',axis=1)
+    metric_columns = list(score_data.columns.values)
+
+    labels = find_correlation_clusters(score_data.corr(),group_corr_thresh)
+    labeled_column_df, relabled_count = relabel_clusters(labels,metric_columns)
+    loadmat_df = make_load_matrix(labeled_column_df, metric_columns, relabled_count)
+
     if save:
-        print('saving loadings')
-        loadmat_df.to_csv(data_set_path.replace('.csv', '_load_mat.csv'))
+        save_path = data_set_path.replace('.csv', '_load_mat.csv')
+        print('saving loadings to ' + save_path)
+        loadmat_df.to_csv(save_path)
 
     return loadmat_df
