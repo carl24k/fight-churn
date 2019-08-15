7f1aec737cd880acd75ffe0b75fdc116a42642f2
Gold, Carl <carl.gold@zuora.com>
2019-08-15 07:04:23 -0700

    Renaming the binding variables

--- listings/chap7/listing_7_1_scaled_events_per_month.sql
+++ listings/chap7/listing_7_1_scaled_events_per_month.sql
@@ -3,11 +3,11 @@ with date_vals AS (
      from generate_series('FRYR-MM-DD', 'TOYR-MM-DD', '7 day'::interval) i
 )
 select account_id, metric_date, 
-(28)::float/(84)::float * count(*) as n_login     
+((QUOTE_PERIOD)::float/(OBS_PERIOD)::float) * count(*) as EVENT_TO_QUERY_QUOTE_PERIODday_avg_OBS_PERIODday_obs
 from event e inner join date_vals d
 on e.event_time <= metric_date 
-and e.event_time > metric_date - interval 'OBS_INTERVAL'
+and e.event_time > metric_date - interval 'OBS_PERIOD days'
 inner join event_type t on t.event_type_id=e.event_type_id
-where t.event_type_name='EVENT_NAME'
+where t.event_type_name='EVENT_TO_QUERY'
 group by account_id, metric_date
-order by account_id, metric_date;
+order by metric_date, account_id;
--- listings/chap7/listing_7_2_tenure_scaled_events_per_month.sql
+++ listings/chap7/listing_7_2_tenure_scaled_events_per_month.sql
@@ -1,14 +1,14 @@
 select m.account_id, metric_time, 
     m.metric_value as tenure_metric,    
     count(*) as count_unscaled,    
-    (28.0/ least(OBS_DAYS,m.metric_value))  as scaling,
-    (28.0/ least(OBS_DAYS,m.metric_value))  * count(*) as EVENT_NAME_permonth_OBS_DAYSday
+    (QUOTE_PERIOD/ least(OBS_PERIOD,m.metric_value))  as scaling,
+    (QUOTE_PERIOD/ least(OBS_PERIOD,m.metric_value))  * count(*) as EVENT_TO_QUERY_permonth_QUOTE_PERIODday_avg
 from event e inner join metric m    on m.account_id = e.account_id
     and event_time <= metric_time
-    and event_time >  metric_time-interval 'OBS_DAYS days'
+    and event_time >  metric_time-interval 'OBS_PERIOD days'
 inner join event_type t on t.event_type_id=e.event_type_id
 inner join metric_name  n on m.metric_name_id = n.metric_name_id
-where t.event_type_name='EVENT_NAME'
+where t.event_type_name='EVENT_TO_QUERY'
     and n.metric_name='account_tenure'
     and metric_value >= MIN_TENURE
 group by m.account_id, metric_time, metric_value    
