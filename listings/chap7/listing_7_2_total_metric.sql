8464f5d502ebbae505fe69533459e243510f285c
Gold, Carl <carl.gold@zuora.com>
2019-08-23 11:47:52 -0500

    Add an illustration column because its not for inserting anyway

--- listings/chap7/listing_7_2_total_metric.sql
+++ listings/chap7/listing_7_2_total_metric.sql
@@ -1,4 +1,6 @@
-select account_id, metric_time, sum(metric_value) as metric_total
+select account_id, metric_time,
+    string_agg(metric_value::text,'+') as metric_sum,
+    sum(metric_value) as metric_total
 from metric m inner join metric_name n on n.metric_name_id=m.metric_name_id
 and n.metric_name in (METRIC_LIST)
 where metric_time between 'FRYR-MM-DD' and 'TOYR-MM-DD'
