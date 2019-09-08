9a7014c2b748ed0630da76b14cb61b674a1aa651
Gold, Carl <carl.gold@zuora.com>
2019-09-08 16:35:03 -0700

    Update names in dataset create to reflect the new model

--- listings/chap4/listing_4_5_data_set_create.sql
+++ listings/chap4/listing_4_5_data_set_create.sql
@@ -5,14 +5,15 @@ with observation_params as
     'TOYR-MM-DD'::timestamp as obs_end    
 )
 select m.account_id, o.observation_date, is_churn,
-sum(case when metric_name_id=0 then metric_value else 0 end) as post_per_month,
+sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
 sum(case when metric_name_id=1 then metric_value else 0 end) as account_tenure,
-sum(case when metric_name_id=2 then metric_value else 0 end) as new_friend_per_month,
-sum(case when metric_name_id=3 then metric_value else 0 end) as like_per_month,
-sum(case when metric_name_id=4 then metric_value else 0 end) as view_feed_per_month,
-sum(case when metric_name_id=5 then metric_value else 0 end) as photo_per_month,
-sum(case when metric_name_id=6 then metric_value else 0 end) as message_per_month,
-sum(case when metric_name_id=7 then metric_value else 0 end) as unfriend_per_month
+sum(case when metric_name_id=2 then metric_value else 0 end) as newfriend_per_month,
+sum(case when metric_name_id=3 then metric_value else 0 end) as post_per_month,
+sum(case when metric_name_id=4 then metric_value else 0 end) as adview_feed_per_month,
+sum(case when metric_name_id=5 then metric_value else 0 end) as dislike_per_month,
+sum(case when metric_name_id=6 then metric_value else 0 end) as unfriend_per_month,
+sum(case when metric_name_id=7 then metric_value else 0 end) as message_per_month,
+sum(case when metric_name_id=8 then metric_value else 0 end) as reply_per_month
 from metric m inner join observation_params
 on metric_time between obs_start and obs_end    
 inner join observation o on m.account_id = o.account_id
