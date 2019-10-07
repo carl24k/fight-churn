fb6de9c9c8a1c9fde24363bdb2d8e2ac73528461
Carl Gold <carl.steven.gold@gmail.com>
2019-10-07 04:21:38 -0700

    Fix in how the output name is labeled

--- listings/chap3/listing_3_1_event_count.sql
+++ listings/chap3/listing_3_1_event_count.sql
@@ -1,7 +1,7 @@
 with calc_date as (
 	select 'TOYR-MM-DD'::timestamp  as the_date   
 ) 
-select account_id, count(*) as n_EVENT_NAME
+select account_id, count(*) as n_event_to_query
 from event e inner join calc_date d on
   e.event_time <= d.the_date
   and e.event_time > d.the_date - interval '28 day'
