036ef759cbbf545ed8b015d398d9b57d9862162b
Gold, Carl <carl.gold@zuora.com>
2019-10-31 15:30:13 -0700

    Current dataset query updated with the chapter 7 metrics

---
+++ listings/chap8/listing_8_2_current_dataset.sql
@@ -0,0 +1,24 @@
+with metric_date as
+(
+    select  max(metric_time) as last_metric_time from metric
+)
+select s.account_id, metric_time,
+sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
+sum(case when metric_name_id=1 then metric_value else 0 end) as newfriend_per_month,
+sum(case when metric_name_id=2 then metric_value else 0 end) as post_per_month,
+sum(case when metric_name_id=3 then metric_value else 0 end) as adview_per_month,
+sum(case when metric_name_id=4 then metric_value else 0 end) as dislike_per_month,
+sum(case when metric_name_id=27 then metric_value else 0 end) as unfriend_per_month,
+sum(case when metric_name_id=6 then metric_value else 0 end) as message_per_month,
+sum(case when metric_name_id=7 then metric_value else 0 end) as reply_per_month,
+sum(case when metric_name_id=8 then metric_value else 0 end) as account_tenure,
+sum(case when metric_name_id=21 then metric_value else 0 end) as adview_per_post,
+sum(case when metric_name_id=23 then metric_value else 0 end) as dislike_pcnt,
+sum(case when metric_name_id=24 then metric_value else 0 end) as newfriend_pcnt_chng,
+sum(case when metric_name_id=25 then metric_value else 0 end) as days_since_newfriend
+from metric m inner join metric_date on metric_time =last_metric_time
+inner join subscription s on m.account_id=s.account_id
+where s.start_date <= last_metric_time
+and (s.end_date >=last_metric_time or s.end_date is null)
+group by s.account_id, metric_time
+order by s.account_id
