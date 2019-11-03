8b6a292b6c06e85fb38f2fec2fc094b1d5863edc
Gold, Carl <carl.gold@zuora.com>
2019-11-03 06:04:08 -0800

    Switch back to old dataset

--- listings/chap8/listing_8_2_dataset_current.sql
+++ listings/chap8/listing_8_2_dataset_current.sql
@@ -9,15 +9,15 @@ account_tenures as (
     and metric_value >= 14
 )
 select s.account_id, last_metric_time as observation_date,
-sum(case when metric_name_id=30 then metric_value else 0 end) as like_per_month,
-sum(case when metric_name_id=31 then metric_value else 0 end) as newfriend_per_month,
-sum(case when metric_name_id=32 then metric_value else 0 end) as post_per_month,
-sum(case when metric_name_id=33 then metric_value else 0 end) as adview_per_month,
-sum(case when metric_name_id=34 then metric_value else 0 end) as dislike_per_month,
-sum(case when metric_name_id=35 then metric_value else 0 end) as unfriend_per_month,
-sum(case when metric_name_id=36 then metric_value else 0 end) as message_per_month,
-sum(case when metric_name_id=37 then metric_value else 0 end) as reply_per_month,
-account_tenure as account_tenure,
+sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
+sum(case when metric_name_id=1 then metric_value else 0 end) as newfriend_per_month,
+sum(case when metric_name_id=2 then metric_value else 0 end) as post_per_month,
+sum(case when metric_name_id=3 then metric_value else 0 end) as adview_per_month,
+sum(case when metric_name_id=4 then metric_value else 0 end) as dislike_per_month,
+sum(case when metric_name_id=27 then metric_value else 0 end) as unfriend_per_month,
+sum(case when metric_name_id=6 then metric_value else 0 end) as message_per_month,
+sum(case when metric_name_id=7 then metric_value else 0 end) as reply_per_month,
+sum(case when metric_name_id=8 then metric_value else 0 end) as account_tenure,
 sum(case when metric_name_id=21 then metric_value else 0 end) as adview_per_post,
 sum(case when metric_name_id=23 then metric_value else 0 end) as dislike_pcnt,
 sum(case when metric_name_id=24 then metric_value else 0 end) as newfriend_pcnt_chng,
@@ -27,5 +27,5 @@ inner join account_tenures a on a.account_id = m.account_id
 inner join subscription s on m.account_id=s.account_id
 where s.start_date <= last_metric_time
 and (s.end_date >=last_metric_time or s.end_date is null)
-group by s.account_id, last_metric_time, account_tenure
+group by s.account_id, last_metric_time
 order by s.account_id
