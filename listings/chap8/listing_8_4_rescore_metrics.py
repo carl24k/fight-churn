5d64530c88581dc412fb48931db78a97ed133e62
Gold, Carl <carl.gold@zuora.com>
2019-11-10 17:17:57 -0800

    Reorganize putting helper functions at the bottom

--- listings/chap8/listing_8_4_rescore_metrics.py
+++ listings/chap8/listing_8_4_rescore_metrics.py
@@ -1,17 +1,9 @@
 import pandas as pd
 import numpy as np
 import os
-
 from listing_7_5_fat_tail_scores import transform_fattail_columns, transform_skew_columns
 
-def reload_churn_data(data_set_path,suffix,listing,is_customer_data):
-    data_path = data_set_path.replace('.csv', '_{}.csv'.format(suffix))
-    assert os.path.isfile(data_path),'You must run listing {} to save {} first'.format(listing,suffix)
-    ic = [0,1] if is_customer_data else 0
-    churn_data = pd.read_csv(data_path, index_col=ic)
-    return churn_data
-
-def rescore_metrics(data_set_path='', save=True):
+def rescore_metrics(data_set_path=''):
 
     load_mat_df = reload_churn_data(data_set_path,'load_mat','6.4',is_customer_data=False)
     score_df = reload_churn_data(data_set_path,'score_params','7.5',is_customer_data=False)
@@ -20,22 +12,24 @@ def rescore_metrics(data_set_path='', save=True):
     assert set(load_mat_df.index.values)==set(current_data.columns.values),"Data to re-score does not match load matrix"
 
     transform_skew_columns(current_data,score_df[score_df['skew_score']].index.values)
-
     transform_fattail_columns(current_data,score_df[score_df['skew_score']].index.values)
 
     current_data=current_data[score_df.index.values]
-
     scaled_data=(current_data-score_df['mean'])/score_df['std']
 
     scaled_data = scaled_data[load_mat_df.index.values]
     grouped_ndarray = np.matmul(scaled_data.to_numpy(), load_mat_df.to_numpy())
 
-    grouped_column_names = ['metric_group_%d' % (d + 1) for d in range(0, load_mat_df.shape[1])]
-    current_data_grouped = pd.DataFrame(grouped_ndarray,columns=grouped_column_names, index=current_data.index)
+    current_data_grouped = pd.DataFrame(grouped_ndarray,columns=load_mat_df.columns.values, index=current_data.index)
 
-    if save:
-        score_save_path=data_set_path.replace('.csv','_current_groupscore.csv')
-        current_data_grouped.to_csv(score_save_path,header=True)
-        print('Saving results to %s' % score_save_path)
+    score_save_path=data_set_path.replace('.csv','_current_groupscore.csv')
+    current_data_grouped.to_csv(score_save_path,header=True)
+    print('Saving results to %s' % score_save_path)
 
-    return current_data_grouped
+
+def reload_churn_data(data_set_path,suffix,listing,is_customer_data):
+    data_path = data_set_path.replace('.csv', '_{}.csv'.format(suffix))
+    assert os.path.isfile(data_path),'You must run listing {} to save {} first'.format(listing,suffix)
+    ic = [0,1] if is_customer_data else 0
+    churn_data = pd.read_csv(data_path, index_col=ic)
+    return churn_data
--- listings/chap8/listing_8_5_churn_forecast.py
+++ listings/chap8/listing_8_5_churn_forecast.py
@@ -1,41 +1,37 @@
 import pandas as pd
-import numpy as np
 import os
 import pickle
 import matplotlib.pyplot as plt
+from listing_8_4_rescore_metrics import reload_churn_data
 
-
-def churn_forecast(data_set_path='',save=True):
+def churn_forecast(data_set_path=''):
 
     pickle_path = data_set_path.replace('.csv', '_logreg_model.pkl')
     assert os.path.isfile(pickle_path), 'You must run listing 8.2 to save a logistic regression model first'
     with open(pickle_path, 'rb') as fid:
         logreg_model = pickle.load(fid)
 
-    score_save_path=data_set_path.replace('.csv','_current_groupscore.csv')
-    assert os.path.isfile(score_save_path), 'You must run listing 8.3 to save current scores first'
-    current_score_df=pd.read_csv(score_save_path,index_col=[0,1])
+    current_score_df = reload_churn_data(data_set_path,'current_groupscore','8.3',is_customer_data=True)
 
     predictions = logreg_model.predict_proba(current_score_df.to_numpy())
 
-    if save:
-        predict_df = pd.DataFrame(predictions, index=current_score_df.index, columns=['churn_prob', 'retain_prob'])
-        forecast_save_path = data_set_path.replace('.csv', '_current_predictions.csv')
-        print('Saving results to %s' % forecast_save_path)
-        predict_df.to_csv(forecast_save_path, header=True)
+    predict_df = pd.DataFrame(predictions, index=current_score_df.index, columns=['churn_prob', 'retain_prob'])
+    forecast_save_path = data_set_path.replace('.csv', '_current_predictions.csv')
+    print('Saving results to %s' % forecast_save_path)
+    predict_df.to_csv(forecast_save_path, header=True)
 
-        plt.figure(figsize=[6,4])
-        n, bins,_ = plt.hist(predictions[:,0],bins=20)
-        hist_df=pd.DataFrame({'n':n,'bins':bins[1:]})
-        plt.xlim(left=0)
-        plt.xlabel('Churn Probability')
-        plt.ylabel('# of Accounts')
-        plt.title('Histogram of Current Churn Probability')
-        # plt.gca().get_yaxis().set_ticklabels([])
-        # plt.gca().get_xaxis().set_ticklabels([])
-        plt.grid()
-        plt.savefig(data_set_path.replace('.csv', '_current_churnhist.svg'), format='svg')
-        plt.close()
-        hist_df.to_csv(data_set_path.replace('.csv', '_current_churnhist.csv'))
+    forecast_histogram(data_set_path,predictions)
 
-    return predictions
+def forecast_histogram(data_set_path,predictions):
+    plt.figure(figsize=[6,4])
+    n, bins,_ = plt.hist(predictions[:,0],bins=20)
+    plt.xlabel('Churn Probability')
+    plt.ylabel('# of Accounts')
+    plt.title('Histogram of Active Customer Churn Probability')
+    # plt.gca().get_yaxis().set_ticklabels([])
+    # plt.gca().get_xaxis().set_ticklabels([])
+    plt.grid()
+    plt.savefig(data_set_path.replace('.csv', '_current_churnhist.png'), format='png')
+    plt.close()
+    hist_df=pd.DataFrame({'n':n,'bins':bins[1:]})
+    hist_df.to_csv(data_set_path.replace('.csv', '_current_churnhist.csv'))
