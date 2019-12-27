3166c0c53fd8cd9cbf7190aef72ada71a43b0cfc
Gold, Carl <carl.gold@zuora.com>
2019-12-27 14:46:48 -0800

    Include subscription constraint in the chapter 4 current customer query

--- listings/chap4/listing_4_6_current_customers.sql
+++ listings/chap4/listing_4_6_current_customers.sql
@@ -2,7 +2,7 @@ with metric_date as
 (
     select  max(metric_time) as last_metric_time from metric
 )
-select account_id, metric_time,
+select m.account_id, metric_time,
 sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
 sum(case when metric_name_id=1 then metric_value else 0 end) as newfriend_per_month,
 sum(case when metric_name_id=2 then metric_value else 0 end) as post_per_month,
@@ -12,6 +12,9 @@ sum(case when metric_name_id=5 then metric_value else 0 end) as unfriend_per_mon
 sum(case when metric_name_id=6 then metric_value else 0 end) as message_per_month,
 sum(case when metric_name_id=7 then metric_value else 0 end) as reply_per_month,
 sum(case when metric_name_id=8 then metric_value else 0 end) as account_tenure
-from metric inner join metric_date on metric_time =last_metric_time
-group by account_id, metric_time
-order by account_id
+from metric m inner join metric_date on metric_time =last_metric_time
+inner join subscription s on m.account_id=s.account_id
+where s.start_date <= last_metric_time
+and (s.end_date >=last_metric_time or s.end_date is null)
+group by m.account_id, metric_time
+order by m.account_id
