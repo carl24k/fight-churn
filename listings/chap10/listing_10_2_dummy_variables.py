713c7300a3fcf539cae66ae98a5f78b272f07b00
Gold, Carl <carl.gold@zuora.com>
2020-01-07 16:45:20 -0800

    Listing to make dummy variables

---
+++ listings/chap10/listing_10_2_dummy_variables.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def dummy_variables(data_set_path,columns):
+
+    raw_data = pd.read_csv(data_set_path, index_col=[0, 1])
+    new_data = raw_data.drop(columns,axis=1)
+
+    for c in columns:
+        dummies = raw_data[c].str.get_dummies()
+        dummies.columns=[str(cat) for cat in dummies.columns]
+        new_data = new_data.join(dummies)
+
+
+    save_path = data_set_path.replace('.csv', '_dummies.csv')
+    new_data.to_csv(save_path,header=True)
+    print('Saved data with dummies  to ' + save_path)
+
