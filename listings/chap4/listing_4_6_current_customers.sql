bc4640efaa6d96ee50f31205e6f6f63be46179cc
Gold, Carl <carl.gold@zuora.com>
2020-01-28 14:58:51 -0800

    Changed my mind - just introduce this element in chapter 8

--- listings/chap4/listing_4_6_current_customers.sql
+++ listings/chap4/listing_4_6_current_customers.sql
@@ -1,12 +1,6 @@
 with metric_date as
 (
     select  max(metric_time) as last_metric_time from metric
-),
-account_tenures as (
-    select account_id, metric_value as account_tenure
-    from metric m inner join metric_date on metric_time =last_metric_time
-    where metric_name_id = 8
-    and metric_value >= 14
 )
 select m.account_id, d.last_metric_time,
 sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
@@ -20,7 +14,6 @@ sum(case when metric_name_id=7 then metric_value else 0 end) as reply_per_month,
 sum(case when metric_name_id=8 then metric_value else 0 end) as account_tenure
 from metric m inner join metric_date d on m.metric_time = d.last_metric_time
 inner join subscription s on m.account_id=s.account_id
-inner join account_tenures a on a.account_id = m.account_id
 where s.start_date <= d.last_metric_time
 and (s.end_date >= d.last_metric_time or s.end_date is null)
 group by m.account_id, d.last_metric_time
