3d464edb1905f9e42fedc271e7d973c8552f74a8
Gold, Carl <carl.gold@zuora.com>
2020-02-04 15:13:54 -0800

    Fix event count plot name and add the configurations for the other events

--- listings/chap3/listing_3_10_event_count_plot.py
+++ listings/chap3/listing_3_10_event_count_plot.py
@@ -4,7 +4,8 @@ from math import ceil
 import  datetime
 
 def event_count_plot(qa_data_path, event_name,**kwargs):
-    qa_data_df=pd.read_csv(qa_data_path + '_' + event_name + '.csv')
+    event_data_path = qa_data_path + '_' + event_name + '.csv'
+    qa_data_df=pd.read_csv(event_data_path)
     plt.figure(figsize=(6, 4))
     plt.plot('event_date', 'n_event', data=qa_data_df, marker='', color='black', linewidth=2)
     plt.ylim(0, ceil(1.1 * qa_data_df['n_event'].dropna().max()))
@@ -12,5 +13,5 @@ def event_count_plot(qa_data_path, event_name,**kwargs):
     plt.gca().figure.autofmt_xdate()
     plt.xticks(list(filter(lambda x:x.endswith(("01")),qa_data_df['event_date'].tolist())))
     plt.tight_layout()
-    plt.savefig(qa_data_path.replace('.csv', '_' + event_name + '_event_qa.png'))
+    plt.savefig(event_data_path.replace('.csv', '_' + event_name + '_event_qa.png'))
     plt.close()
