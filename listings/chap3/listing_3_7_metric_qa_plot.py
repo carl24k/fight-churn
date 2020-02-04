75f9c531e48bec75e1279d096acb64734cac8619
Gold, Carl <carl.gold@zuora.com>
2020-02-04 11:19:48 -0800

    Refactor to make subplots with a function

--- listings/chap3/listing_3_7_metric_qa_plot.py
+++ listings/chap3/listing_3_7_metric_qa_plot.py
@@ -5,28 +5,18 @@ import os
 
 def metric_qa_plot(qa_data_path, metric_name,**kwargs):
     qa_data_df=pd.read_csv(qa_data_path)
-
-    plt.figure(figsize=(8, 10))
-    plt.subplot(4, 1, 1)
-    plt.plot('calc_date', 'max', data=qa_data_df, marker='', color='black', linewidth=2, label="max")
-    plt.ylim(0, ceil(1.1 * qa_data_df['max'].dropna().max()))
-    plt.legend()
+    plt.figure(figsize=(6, 6))
+    qa_subplot(qa_data_df,'max',1,None)
     plt.title(metric_name)
-    plt.subplot(4, 1, 2)
-    plt.plot('calc_date', 'avg', data=qa_data_df, marker='',linestyle='--', color='black', linewidth=2, label='avg')
-    plt.ylim(0, ceil(1.1 * qa_data_df['avg'].dropna().max()))
-    plt.legend()
-    plt.subplot(4, 1, 3)
-    plt.plot('calc_date', 'min', data=qa_data_df, marker='',linestyle='-.',  color='black', linewidth=2, label='min')
-    plt.ylim(0, ceil(2*qa_data_df['min'].dropna().max()))
-    plt.legend()
-    plt.subplot(4, 1, 4)
-    plt.plot('calc_date', 'n_calc', data=qa_data_df, marker='',linestyle=':',  color='black', linewidth=2, label="n_calc")
-    plt.ylim(0, ceil(1.1 * qa_data_df['n_calc'].dropna().max()))
-    plt.legend()
+    qa_subplot(qa_data_df,'avg',2,'--')
+    qa_subplot(qa_data_df,'min',3,'-.')
+    qa_subplot(qa_data_df,'n_calc',4,':')
     plt.gca().figure.autofmt_xdate()
-
-    plt.gcf().autofmt_xdate()
-
     plt.savefig(qa_data_path.replace('.csv', '_' + metric_name + '_churn_qa.png'))
     plt.close()
+
+def qa_subplot(qa_data_df, field, number, linestyle):
+    plt.subplot(4, 1, number)
+    plt.plot('calc_date', field, data=qa_data_df, marker='', linestyle=linestyle, color='black', linewidth=2, label=field)
+    plt.ylim(0, ceil(1.1 * qa_data_df[field].dropna().max()))
+    plt.legend()
