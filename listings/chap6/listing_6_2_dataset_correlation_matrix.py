7546a8bb367d93c7114762d18552d3376a2e1a08
Gold, Carl <carl.gold@zuora.com>
2020-03-11 04:57:32 -0700

    Avoid name collision

--- listings/chap6/listing_6_2_dataset_correlation_matrix.py
+++ listings/chap6/listing_6_2_dataset_correlation_matrix.py
@@ -4,8 +4,8 @@ def dataset_correlation_matrix(data_set_path):
 
     churn_data = pd.read_csv(data_set_path,index_col=[0,1])
     churn_data = churn_data.reindex(sorted(churn_data.columns), axis=1)
-    corr = churn_data.corr()
+    corr_df = churn_data.corr()
 
     save_name = data_set_path.replace('.csv', '_correlation_matrix.csv')
-    corr.to_csv(save_name)
+    corr_df.to_csv(save_name)
     print('Saved correlation matrix to ' + save_name)
