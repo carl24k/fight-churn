eebda01c0bc6892c4d9fece8122037ca5cfce14b
Carl Gold <carl.steven.gold@gmail.com>
2020-07-21 09:00:17 -0700

    More progress in the livebook script, and modifcations to the metric coverage query

--- listings/chap3/listing_3_8_metric_coverage.sql
+++ listings/chap3/listing_3_8_metric_coverage.sql
@@ -4,9 +4,8 @@ date_range as (
 		'%to_yyyy-mm-dd'::timestamp as end_date
 ), account_count as (    
 	select count(distinct account_id) as n_account    
-	from subscription s inner join date_range d on
-	 s.start_date <= d.end_date
-	and (s.end_date >= d.start_date or s.end_date is null)
+	from event e inner join date_range d on
+	 e.event_time between start_date and end_date
 )
 select metric_name, 
 	count(distinct m.account_id) as count_with_metric,
@@ -22,8 +21,5 @@ inner join date_range on
 	metric_time >= start_date
 	and metric_time <= end_date
 inner join metric_name  n on m.metric_name_id = n.metric_name_id
-inner join subscription s on s.account_id = m.account_id
-    and s.start_date <= m.metric_time
-    and (s.end_date >= m.metric_time or s.end_date is null)
 group by metric_name,n_account
 order by metric_name;
--- listings/py/run_livebook_listings.py
+++ listings/py/run_livebook_listings.py
@@ -8,7 +8,6 @@ params = {
 }
 
 py_params = {
-    "qa_data_path": "../../../fight-churn-output/livebook/livebook_events_per_day",
     "event_name": "like"
 }
 
@@ -24,8 +23,30 @@ events = ['ReadingOwnedBook',
     'ReadingOpenChapter'
 ]
 
-for e in events:
-    params['%event2measure']=e
-    sql_listing(3, 9, 'events_per_day', 'livebook', mode='save', param_dict=params, save_ext=e)
-    py_params["event_name"]=e
-    python_listing(3,10,'event_count_plot',param_dict=py_params)
+if False:
+    for e in events:
+        params['%event2measure']=e
+        sql_listing(3, 9, 'events_per_day', 'livebook', mode='save', param_dict=params, save_ext=e)
+        py_params["event_name"]=e
+        py_params["qa_data_path"]="../../../fight-churn-output/livebook/livebook_events_per_day",
+        python_listing(3,10,'event_count_plot',param_dict=py_params)
+
+
+if False:
+    for idx,e in enumerate(events):
+        params['%event2measure']=e
+        params['%new_metric_id']=idx
+        sql_listing(3, 3, 'count_metric_insert', 'livebook', mode='run', param_dict=params)
+        params["%new_metric_name"]=f'{e}_90d'
+        sql_listing(3,4,'metric_name_insert','livebook',mode='run',param_dict=params)
+
+if False:
+    for e in events:
+        m = f'{e}_90d'
+        params["%metric2measure"]=m
+        sql_listing(3,6,'metric_stats_over_time','livebook',mode='save',param_dict=params,save_ext=m)
+        py_params["metric_name"] = m
+        py_params["qa_data_path"]  = "../../../fight-churn-output/livebook/livebook_metric_stats_over_time"
+        python_listing(3, 7, 'metric_qa_plot', param_dict=py_params)
+
+sql_listing(3,8,'metric_coverage','livebook',mode='save',param_dict=params)
