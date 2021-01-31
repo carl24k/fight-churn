858fa8d1a09f3fd3fb2ce349698a81b83d4326cb
Carl Gold <carl.steven.gold@gmail.com>
2021-01-31 13:58:40 -0800

    First milestone with JSON configuration

--- data-generation/schema/create_observation.sql
+++ data-generation/schema/create_observation.sql
@@ -1,22 +1,28 @@
-CREATE TABLE IF NOT EXISTS  x.observation
+-- Table: livebook.observation
+CREATE TABLE livebook.observation
 (
-    account_id integer NOT NULL,
+    account_id character(32) COLLATE pg_catalog."default" NOT NULL,
     observation_date date NOT NULL,
-    is_churn boolean NULL)
+    is_churn boolean
+)
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE x.observation
+ALTER TABLE livebook.observation
     OWNER to postgres;
 
-CREATE INDEX IF NOT EXISTS  observation_date_idx
-    ON x.observation USING btree
-    (observation_date)
+-- Index: idx_observation_account_date
+
+CREATE UNIQUE INDEX idx_observation_account_date
+    ON livebook.observation USING btree
+    (account_id COLLATE pg_catalog."default", observation_date)
     TABLESPACE pg_default;
 
-CREATE UNIQUE INDEX IF NOT EXISTS  idx_observation_account_date
-    ON x.observation USING btree
-    (account_id, observation_date)
+-- Index: observation_date_idx
+
+CREATE INDEX observation_date_idx
+    ON livebook.observation USING btree
+    (observation_date)
     TABLESPACE pg_default;
---
+++ listings/chap4/listing_4_7_livebook_observations.sql
@@ -0,0 +1,20 @@
+with first_use as (
+	select account_id, product_id, min(event_time) as first_use_time
+	from event
+	where event_type in ('ReadingOwnedBook','EBookDownloaded')
+	group by account_id, product_id
+	order by account_id, first_use_time
+),
+pre_0301_accounts as (
+	select distinct account_id from first_use
+	where first_use_time < cast('2020-03-01' as timestamp)
+),
+post_0301_accounts as (
+	select distinct account_id from first_use
+	where first_use_time >= cast('2020-03-01' as timestamp)
+)
+insert into observation (account_id, observation_date, is_churn)
+select pre.account_id, cast('2020-03-01' as date) as observation_date,
+case when post.account_id is null then true else false end as is_churn
+from pre_0301_accounts pre left outer join post_0301_accounts post
+on pre.account_id = post.account_id;
