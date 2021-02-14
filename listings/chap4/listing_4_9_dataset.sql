db2d2f990d6196ab3b31ed538782dfc0cea32850
Carl Gold <carl.steven.gold@gmail.com>
2021-02-14 15:49:34 -0800

    Add new metrics and basic churn cohort plots

--- listings/chap4/listing_4_9_dataset.sql
+++ listings/chap4/listing_4_9_dataset.sql
@@ -1,12 +1,14 @@
 select m.account_id, m.metric_time, is_churn,
-sum(case when metric_name_id=0 then metric_value else 0 end) as ReadingOwnedBook_90d,
-sum(case when metric_name_id=1 then metric_value else 0 end) as EBookDownloaded_90d,
-sum(case when metric_name_id=2 then metric_value else 0 end) as ReadingFreePreview_90d,
-sum(case when metric_name_id=3 then metric_value else 0 end) as HighlightCreated_90d,
-sum(case when metric_name_id=4 then metric_value else 0 end) as FreeContentCheckout_90d,
-sum(case when metric_name_id=5 then metric_value else 0 end) as ReadingOpenChapter_90d,
-sum(case when metric_name_id=6 then metric_value else 0 end) as WishlistItemAdded_90d,
-sum(case when metric_name_id=7 then metric_value else 0 end) as CrossReferenceTermOpened_90d
+sum(case when metric_name_id=0 then metric_value else 0 end) as ReadingOwnedBook,
+sum(case when metric_name_id=1 then metric_value else 0 end) as EBookDownloaded,
+sum(case when metric_name_id=2 then metric_value else 0 end) as ReadingFreePreview,
+sum(case when metric_name_id=3 then metric_value else 0 end) as HighlightCreated,
+sum(case when metric_name_id=4 then metric_value else 0 end) as FreeContentCheckout,
+sum(case when metric_name_id=5 then metric_value else 0 end) as ReadingOpenChapter,
+sum(case when metric_name_id=6 then metric_value else 0 end) as WishlistItemAdded,
+sum(case when metric_name_id=7 then metric_value else 0 end) as CrossReferenceTermOpened,
+sum(case when metric_name_id=8 then metric_value else 0 end) as TotalEvents,
+sum(case when metric_name_id=9 then metric_value else 0 end) as DistinctProducts
 from metric m
 inner join observation o on m.account_id = o.account_id
     and m.metric_time > (o.observation_date - 7)::timestamp
--- listings/py/livebook_churn.py
+++ listings/py/livebook_churn.py
@@ -1,3 +1,4 @@
+from listing_5_1_cohort_plot import cohort_plot
 from listing_5_2_dataset_stats import dataset_stats
 from run_churn_listing import sql_listing
 
@@ -11,7 +12,22 @@ def dataset():
     sql_listing(4, 9, 'dataset', 'livebook', mode='save', param_dict={})
     dataset_stats(livebook_data_path)
 
+def cohorts():
+    metrics = ['readingownedbook',
+                'ebookdownloaded',
+                'readingfreepreview',
+                'highlightcreated',
+                'freecontentcheckout',
+                'readingopenchapter',
+                'wishlistitemadded',
+                'crossreferencetermopened',
+                'totalevents',
+                'distinctproducts']
+    for m in metrics:
+        cohort_plot(livebook_data_path,m)
+
 
 if __name__ == "__main__":
-    observations()
-    dataset()
+    # observations()
+    # dataset()
+    cohorts()
