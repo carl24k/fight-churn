c5fc146e3d96b5600c66e1b0a9795bfe6b821af3
Carl Gold <carl.steven.gold@gmail.com>
2021-03-20 13:59:15 -0700

    More changes to finish project 2

--- listings/chap4/listing_4_9_dataset.sql
+++ listings/chap4/listing_4_9_dataset.sql
@@ -7,8 +7,13 @@ sum(case when metric_name_id=4 then metric_value else 0 end) as FreeContentCheck
 sum(case when metric_name_id=5 then metric_value else 0 end) as ReadingOpenChapter,
 sum(case when metric_name_id=6 then metric_value else 0 end) as WishlistItemAdded,
 sum(case when metric_name_id=7 then metric_value else 0 end) as CrossReferenceTermOpened,
-sum(case when metric_name_id=8 then metric_value else 0 end) as TotalEvents,
-sum(case when metric_name_id=9 then metric_value else 0 end) as DistinctProducts
+sum(case when metric_name_id=8 then metric_value else 0 end) as total_events,
+sum(case when metric_name_id=9 then metric_value else 0 end) as distinct_products,
+sum(case when metric_name_id=10 then metric_value else 0 end) as reading_utilities,
+sum(case when metric_name_id=11 then metric_value else 0 end) as downloads_per_book,
+sum(case when metric_name_id=12 then metric_value else 0 end) as percent_reading,
+sum(case when metric_name_id=13 then metric_value else 0 end) as percent_utilities,
+sum(case when metric_name_id=14 then metric_value else 0 end) as events_per_book
 from metric m
 inner join observation o on m.account_id = o.account_id
     and m.metric_time > (o.observation_date - 7)::timestamp
--- listings/py/livebook_churn.py
+++ listings/py/livebook_churn.py
@@ -1,12 +1,14 @@
 from listing_5_1_cohort_plot import cohort_plot
 from listing_5_2_dataset_stats import dataset_stats
+from listing_6_2_dataset_correlation_matrix import dataset_correlation_matrix
+from listing_3_7_metric_qa_plot import metric_qa_plot
+
 from run_churn_listing import sql_listing
 
 livebook_data_path = '../../../fight-churn-output/livebook/livebook_dataset.csv'
 
 def observations():
-    sql_listing(4, 7, 'create_observation', 'livebook', mode='save', param_dict={})
-    sql_listing(4, 8, 'livebook_observations', 'livebook', mode='save', param_dict={})
+    sql_listing(4, 8, 'livebook_observations', 'livebook', mode='run', param_dict={})
 
 def dataset():
     sql_listing(4, 9, 'dataset', 'livebook', mode='save', param_dict={})
@@ -26,8 +28,82 @@ def cohorts():
     for m in metrics:
         cohort_plot(livebook_data_path,m)
 
+def readingUtilities():
+    params = {
+        "%from_yyyy-mm-dd": "2019-12-01",
+        "%to_yyyy-mm-dd": "2020-06-01",
+        "%new_metric_id" : 10,
+        "%new_metric_name" :'reading_utilities_90d',
+        '%metric_list' : "'ReadingFreePreview_90d','HighlightCreated_90d',  'FreeContentCheckout_90d','ReadingOpenChapter_90d', 'WishlistItemAdded_90d','CrossReferenceTermOpened_90d'"
+    }
+    sql_listing(7, 3, 'total_metric', 'livebook', mode='run', param_dict=params, insert=True)
+
+
+def ratio1():
+    params = {
+        "%from_yyyy-mm-dd": "2019-12-01",
+        "%to_yyyy-mm-dd": "2020-06-01",
+        "%new_metric_id" : 11,
+        "%new_metric_name" :'downloads_per_book',
+        '%num_metric' : 'EBookDownloaded_90d',
+        '%den_metric' :'DistinctProducts'
+    }
+    sql_listing(7,1,'ratio_metric','livebook',mode='run',param_dict=params,insert=True)
+
+def percent_reading():
+    params = {
+        "%from_yyyy-mm-dd": "2019-12-01",
+        "%to_yyyy-mm-dd": "2020-06-01",
+        "%new_metric_id" : 12,
+        "%new_metric_name" :'percent_reading',
+        '%num_metric' : 'ReadingOwnedBook_90d',
+        '%den_metric' :'TotalEvents_90d'
+    }
+    sql_listing(7,1,'ratio_metric','livebook',mode='run',param_dict=params,insert=True)
+
+
+def percent_utilities():
+    params = {
+        "%from_yyyy-mm-dd": "2019-12-01",
+        "%to_yyyy-mm-dd": "2020-06-01",
+        "%new_metric_id" : 13,
+        "%new_metric_name" :'percent_utilities',
+        '%num_metric' : 'reading_utilities_90d',
+        '%den_metric' :'TotalEvents_90d'
+    }
+    sql_listing(7,1,'ratio_metric','livebook',mode='run',param_dict=params,insert=True)
+
+
+def events_per_product():
+    params = {
+        "%from_yyyy-mm-dd": "2019-12-01",
+        "%to_yyyy-mm-dd": "2020-06-01",
+        "%new_metric_id" : 14,
+        "%new_metric_name" :'events_per_product',
+        '%num_metric' : 'TotalEvents_90d',
+        '%den_metric' :'DistinctProducts'
+    }
+    sql_listing(7,1,'ratio_metric','livebook',mode='run',param_dict=params,insert=True)
+
+
+def more_cohorts():
+    cohort_plot(livebook_data_path,'reading_utilities')
+    cohort_plot(livebook_data_path,'downloads_per_book')
+    cohort_plot(livebook_data_path,'percent_reading')
+    cohort_plot(livebook_data_path,'percent_utilities')
+    cohort_plot(livebook_data_path,'events_per_book')
+
+
 
 if __name__ == "__main__":
     # observations()
     # dataset()
-    cohorts()
+    # cohorts()
+    # readingUtilities()
+    # ratio1()
+    # percent_reading()
+    # percent_utilities()
+    # dataset()
+    # more_cohorts()
+    # events_per_product()
+    dataset_correlation_matrix(livebook_data_path)
