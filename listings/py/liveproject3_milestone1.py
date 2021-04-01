36e5cc032ef9cdd957c64ddd95ac772fbc904b11
Carl Gold <carl.steven.gold@gmail.com>
2021-03-31 19:22:52 -0700

    Rewrite in terms of the book listings, including SHAP explenation and forecasting

--- listings/py/liveproject3_milestone1.py
+++ listings/py/liveproject3_milestone1.py
@@ -1,23 +1,9 @@
-import xgboost as xgb
-import pandas as pd
-import numpy as np
+from listing_9_6_crossvalidate_xgb import crossvalidate_xgb
+from listing_9_7_churn_forecast_xgb import churn_forecast_xgb
+from listing_9_8_shap_explain_xgb import shap_explain_xgb
 
 train_data_path = '/Users/carl/Documents/churn/fight-churn-output/livebook/livebook_dataset.csv'
-forecast_data_path = '/Users/carl/Documents/churn/fight-churn-output/livebook/livebook_current_customers.csv'
-forecast_save_path = '/Users/carl/Documents/churn/fight-churn-output/livebook/livebook_customer_forecasts.csv'
 
-
-training_data = pd.read_csv(train_data_path, index_col=[0, 1])
-forecast_data = pd.read_csv(forecast_data_path,index_col=[0,1])
-y = training_data['is_churn'].astype(np.bool)
-X = training_data.drop(['is_churn'], axis=1)
-
-xgb_model = xgb.XGBClassifier(objective='binary:logistic')
-xgb_model.fit(X.values,y)
-
-predictions = xgb_model.predict_proba(forecast_data.values)
-
-predict_df = pd.DataFrame(predictions, index=forecast_data.index, columns=['retain_prob','churn_prob'])
-
-predict_df.to_csv(forecast_save_path, header=True)
-
+crossvalidate_xgb(train_data_path,n_test_split=4)
+shap_explain_xgb(train_data_path)
+churn_forecast_xgb(train_data_path)
