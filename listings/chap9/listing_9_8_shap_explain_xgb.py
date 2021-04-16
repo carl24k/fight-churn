9b9c880cd0d16a044226895b84e262b0766376df
Carl Gold <carl.steven.gold@gmail.com>
2021-04-16 15:55:17 -0700

    Use options to make plot in probability Options to make some specified waterfalls

--- listings/chap9/listing_9_8_shap_explain_xgb.py
+++ listings/chap9/listing_9_8_shap_explain_xgb.py
@@ -4,7 +4,7 @@ import shap
 import matplotlib.pyplot as plt
 from listing_8_4_rescore_metrics import reload_churn_data
 
-def shap_explain_xgb(data_set_path):
+def shap_explain_xgb(data_set_path, plot_n=None):
 
     pickle_path = data_set_path.replace('.csv', '_xgb_model.pkl')
     assert os.path.isfile(pickle_path), 'You must run listing 9.6 to save an XGB regression model first'
@@ -12,7 +12,7 @@ def shap_explain_xgb(data_set_path):
         xgb_model = pickle.load(fid)
 
     current_df = reload_churn_data(data_set_path,'current','8.3',is_customer_data=True)
-    explainer = shap.Explainer(xgb_model)
+    explainer = shap.TreeExplainer(xgb_model,feature_perturbation = "interventional", model_output='probability',data=current_df)
     shap_values = explainer(current_df)
     shap.summary_plot(shap_values, current_df, show=False)
     save_file = data_set_path.replace('.csv', '_shap_summary_xgb.png')
@@ -20,3 +20,11 @@ def shap_explain_xgb(data_set_path):
     plt.tight_layout()
     plt.savefig(save_file, format='png')
     plt.close()
+
+    if plot_n is not None:
+        for n in plot_n:
+            shap.waterfall_plot(shap_values[n,:],show=False)
+            save_file = data_set_path.replace('.csv', f'_shap_water_xgb_{n}.png')
+            plt.tight_layout()
+            plt.savefig(save_file, format='png')
+            plt.close()
