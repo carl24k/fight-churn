d4a45199169b00770d2beef542f784c256b2b1f8
Carl Gold <carl.steven.gold@gmail.com>
2022-10-30 09:58:19 -0700

    Switch to likes per dollar for the workshop Remvoe account tenure from the dataset

--- fightchurn/listings/chap4/listing_4_5_dataset.sql
+++ fightchurn/listings/chap4/listing_4_5_dataset.sql
@@ -5,7 +5,6 @@ with observation_params as
     '%to_yyyy-mm-dd'::timestamp as obs_end
 )
 select m.account_id, o.observation_date, is_churn,
-sum(case when metric_name_id=10 then metric_value else 0 end) as account_tenure,
 sum(case when metric_name_id=11 then metric_value else 0 end) as like_per_month,
 sum(case when metric_name_id=12 then metric_value else 0 end) as newfriend_per_month,
 sum(case when metric_name_id=13 then metric_value else 0 end) as post_per_month,
@@ -15,7 +14,7 @@ sum(case when metric_name_id=16 then metric_value else 0 end) as unfriend_per_mo
 sum(case when metric_name_id=17 then metric_value else 0 end) as message_per_month,
 sum(case when metric_name_id=18 then metric_value else 0 end) as reply_per_month,
 sum(case when metric_name_id=20 then metric_value else 0 end) as mrr,
-sum(case when metric_name_id=21 then metric_value else 0 end) as newfriend_per_dollar
+sum(case when metric_name_id=21 then metric_value else 0 end) as like_per_dollar
 from metric m inner join observation_params
 on metric_time between obs_start and obs_end    
 inner join observation o on m.account_id = o.account_id
