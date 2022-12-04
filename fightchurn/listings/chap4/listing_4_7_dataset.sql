34396dda303f60cac1a37739f966b50398ce798f
Carl Gold <carl.steven.gold@gmail.com>
2022-12-04 07:20:04 -0800

    Fix mixed up data sets

--- fightchurn/listings/chap4/listing_4_7_dataset.sql
+++ fightchurn/listings/chap4/listing_4_7_dataset.sql
@@ -5,15 +5,16 @@ with observation_params as
     '%to_yyyy-mm-dd'::timestamp as obs_end
 )
 select m.account_id, o.observation_date, is_churn,
-sum(case when metric_name_id=0 then metric_value else 0 end) as like_per_month,
-sum(case when metric_name_id=1 then metric_value else 0 end) as newfriend_per_month,
-sum(case when metric_name_id=2 then metric_value else 0 end) as post_per_month,
-sum(case when metric_name_id=3 then metric_value else 0 end) as adview_per_month,
-sum(case when metric_name_id=4 then metric_value else 0 end) as dislike_per_month,
-sum(case when metric_name_id=5 then metric_value else 0 end) as unfriend_per_month,
-sum(case when metric_name_id=6 then metric_value else 0 end) as message_per_month,
-sum(case when metric_name_id=7 then metric_value else 0 end) as reply_per_month,
-sum(case when metric_name_id=8 then metric_value else 0 end) as account_tenure
+sum(case when metric_name_id=11 then metric_value else 0 end) as advance_stage,
+sum(case when metric_name_id=12 then metric_value else 0 end) as close_opportunity,
+sum(case when metric_name_id=13 then metric_value else 0 end) as search,
+sum(case when metric_name_id=14 then metric_value else 0 end) as add_competition,
+sum(case when metric_name_id=15 then metric_value else 0 end) as revert_stage,
+sum(case when metric_name_id=16 then metric_value else 0 end) as lose_opportunity,
+sum(case when metric_name_id=17 then metric_value else 0 end) as meeting_held,
+sum(case when metric_name_id=18 then metric_value else 0 end) as schedule_meeting,
+sum(case when metric_name_id=20 then metric_value else 0 end) as mrr,
+sum(case when metric_name_id=21 then metric_value else 0 end) as opp_close_per_dollar
 from metric m inner join observation_params
 on metric_time between obs_start and obs_end    
 inner join observation o on m.account_id = o.account_id
