f0de02607d998a37d86110016fabe0b3c5b9b8d9
Carl Gold <carl.steven.gold@gmail.com>
2022-12-27 18:19:37 -0800

    Insert active user metric

--- fightchurn/listings/chap7/insert_7_9_count_active_users.sql
+++ fightchurn/listings/chap7/insert_7_9_count_active_users.sql
@@ -1,8 +1,13 @@
+INSERT into metric_name values (%new_metric_id,'active_users')
+ON CONFLICT DO NOTHING;
+
 with date_vals AS (
      select i::timestamp as metric_date
      from generate_series('%from_yyyy-mm-dd', '%to_yyyy-mm-dd', '7 day'::interval) i
 )
-select account_id, metric_date, count(distinct user_id) as n_distinct_users
+insert into metric (account_id,metric_time,metric_name_id,metric_value)
+
+select account_id, metric_date,%new_metric_id, count(distinct user_id)
 from event e inner join date_vals d
 on e.event_time <= metric_date
 and e.event_time > metric_date - interval '%obs_period days'
