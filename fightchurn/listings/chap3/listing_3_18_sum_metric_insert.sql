552a1104e9ad4eb0bda45d37010d19b448e87deb
Carl Gold <carl.steven.gold@gmail.com>
2023-01-14 16:33:28 -0800

    Total event value metric New data set config & rename the old config

---
+++ fightchurn/listings/chap3/listing_3_18_sum_metric_insert.sql
@@ -0,0 +1,13 @@
+with date_vals AS (    
+	select i::timestamp as metric_date 
+	from generate_series('%from_yyyy-mm-dd', '%to_yyyy-mm-dd', '7 day'::interval) i
+)
+insert into metric (account_id,metric_time,metric_name_id, metric_value)
+select account_id, metric_date::date, %new_metric_id as metric_name_id, sum(%field2sum) as metric_value
+from event e inner join date_vals d    
+	on e.event_time < metric_date 
+	and e.event_time >= metric_date - interval '28 day'
+inner join event_type t on t.event_type_id=e.event_type_id
+where t.event_type_name='%event2measure'
+group by account_id, metric_date
+ON CONFLICT DO NOTHING;
---
+++ fightchurn/listings/chap4/listing_4_8_dataset_biznet4.sql
@@ -0,0 +1,26 @@
+with observation_params as     
+(
+    select  interval '%metric_interval' as metric_period,
+    '%from_yyyy-mm-dd'::timestamp as obs_start,
+    '%to_yyyy-mm-dd'::timestamp as obs_end
+)
+select m.account_id, o.observation_date, is_churn,
+sum(case when metric_name_id=11 then metric_value else 0 end) as advance_stage,
+sum(case when metric_name_id=12 then metric_value else 0 end) as close_opportunity,
+sum(case when metric_name_id=13 then metric_value else 0 end) as search,
+sum(case when metric_name_id=14 then metric_value else 0 end) as add_competition,
+sum(case when metric_name_id=15 then metric_value else 0 end) as revert_stage,
+sum(case when metric_name_id=16 then metric_value else 0 end) as lose_opportunity,
+sum(case when metric_name_id=17 then metric_value else 0 end) as meeting_held,
+sum(case when metric_name_id=18 then metric_value else 0 end) as schedule_meeting,
+sum(case when metric_name_id=20 then metric_value else 0 end) as mrr,
+sum(case when metric_name_id=21 then metric_value else 0 end) as opp_close_per_dollar,
+sum(case when metric_name_id=30 then metric_value else 0 end) as active_users_per_month,
+sum(case when metric_name_id=31 then metric_value else 0 end) as total_opp_value
+from metric m inner join observation_params
+on metric_time between obs_start and obs_end    
+inner join observation o on m.account_id = o.account_id
+    and m.metric_time > (o.observation_date - metric_period)::timestamp    
+    and m.metric_time <= o.observation_date::timestamp
+group by m.account_id, metric_time, observation_date, is_churn    
+order by observation_date,m.account_id
