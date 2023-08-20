c7cc19987e80e30ac91732b2b760279d081e1c64
Carl Gold <carl.steven.gold@gmail.com>
2023-08-20 06:48:05 -0700

    Bump the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fightchurn",
-    version="1.1.0",
+    version="1.1.1",
     author="Carl Gold",
     author_email="carl24k@fightchurnwithdata.com",
     description="Code from the book Fighting Churn With Data",
